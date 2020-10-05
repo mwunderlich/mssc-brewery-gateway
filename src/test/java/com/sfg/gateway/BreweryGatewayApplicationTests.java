06fbbf7b334bdf199f34e0aeb6c884034c4f05bc
Martin Wunderlich <martin@wunderlich.com>
2020-10-05 10:29:34 +0200

    Initial commit

---
+++ src/test/java/com/sfg/gateway/BreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
